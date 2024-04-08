bdfd7c1bba5b8e3d7943af2f10e9e155ed2ded09
msaiko230 <mattsaiko@gmail.com>
2024-04-08 09:00:21 -0500

    Update py file for better logging

--- ctx_sync.py
+++ ctx_sync.py
@@ -23,9 +23,9 @@ app_logger = get_logger()
 
 print ("****This application is a proof of concept and is not support by Druid Software****")
 
-def process_detach(imsi, ep_address):
+def process_detach(imsi, ep_address, rm_address):
     #app_logger = get_logger()
-    app_logger.info(f"Attach request for {imsi} in {ep_address}")
+    app_logger.info(f"Attach request for {imsi} in {rm_address}")
     url =str("https://%s:%s@%s/api/subscriber?imsi=%s" % (APIUSER, APIPASS, ep_address, imsi))
     try:
         buffer = io.BytesIO()
@@ -42,30 +42,33 @@ def process_detach(imsi, ep_address):
         c.close()
         if status_code == 200:
             data = json.loads(buffer.getvalue().decode('utf-8'))
-            for item in data:
-                app_logger.info(f"Subscriber {imsi} status in {ep_address} = {item['local_ps_attachment']}")
-                if item['local_ps_attachment'] == "ATTACHED":
-                    app_logger.info(f"Detaching subscriber {imsi} from {ep_address}")
-                    url_imsi =str("https://%s:%s@%s/api/subscriber?imsi=%s" % (APIUSER, APIPASS, ep_address, imsi))
-                    patch_data = {"local_ps_attachment": "DETACHED"}
-                    postfields = urlencode(patch_data)
-                    c = pycurl.Curl()
-                    c.setopt(pycurl.URL, url_imsi)
-                    c.setopt(pycurl.CUSTOMREQUEST, "PATCH")
-                    c.setopt(pycurl.USERNAME, APIUSER)
-                    c.setopt(pycurl.PASSWORD, APIPASS)
-                    c.setopt(pycurl.SSL_VERIFYPEER, 0)
-                    c.setopt(pycurl.SSL_VERIFYHOST, 0)
-                    c.setopt(pycurl.CONNECTTIMEOUT, 10)
-                    c.setopt(pycurl.POSTFIELDS, postfields)
-                    c.perform()
-                    status_code = c.getinfo(pycurl.RESPONSE_CODE)
-                    if status_code == 200:
-                        app_logger.info(f"Subscriber {imsi} detached from {ep_address}")
+            if len(data) != 0:
+                for item in data:
+                    app_logger.info(f"Subscriber {imsi} status in {ep_address} = {item['local_ps_attachment']}")
+                    if item['local_ps_attachment'] == "ATTACHED":
+                        app_logger.info(f"Detaching subscriber {imsi} from {ep_address}")
+                        url_imsi =str("https://%s:%s@%s/api/subscriber?imsi=%s" % (APIUSER, APIPASS, ep_address, imsi))
+                        patch_data = {"local_ps_attachment": "DETACHED"}
+                        postfields = urlencode(patch_data)
+                        c = pycurl.Curl()
+                        c.setopt(pycurl.URL, url_imsi)
+                        c.setopt(pycurl.CUSTOMREQUEST, "PATCH")
+                        c.setopt(pycurl.USERNAME, APIUSER)
+                        c.setopt(pycurl.PASSWORD, APIPASS)
+                        c.setopt(pycurl.SSL_VERIFYPEER, 0)
+                        c.setopt(pycurl.SSL_VERIFYHOST, 0)
+                        c.setopt(pycurl.CONNECTTIMEOUT, 10)
+                        c.setopt(pycurl.POSTFIELDS, postfields)
+                        c.perform()
+                        status_code = c.getinfo(pycurl.RESPONSE_CODE)
+                        if status_code == 200:
+                            app_logger.info(f"Subscriber {imsi} detached from {ep_address}")
+                        else:
+                            app_logger.info(f"Unable to detach subscriber {imsi} from {ep_address}")
                     else:
-                        app_logger.info(f"Unable to detach subscriber {imsi} from {ep_address}")
-                else:
-                    app_logger.info(f"{imsi} already detached in {ep_address}")
+                        app_logger.info(f"{imsi} already detached in {ep_address}")
+            else:
+                app_logger.info(f"Subscriber {imsi} not found in {ep_address}")
         else:
             app_logger.info(f"Failed to fetch data. Status code: {status_code}")
     except pycurl.error as e:
@@ -79,15 +82,15 @@ def webhook():
         if request.remote_addr == EPC2:
             imsi = request.form.get('imsi')
             if imsi is not None:
-                process_detach(imsi, EPC1)
+                process_detach(imsi, EPC1, EPC2)
             else:
-                app_logger.info(f"Subscriber {imsi} not found in {EPC1}")
+                app_logger.info(f"Incorrect event subscriber sent or no imsi found in the data")
         elif request.remote_addr == EPC1:
             imsi = request.form.get('imsi')
             if imsi is not None:
-                process_detach(imsi, EPC2)
+                process_detach(imsi, EPC2, EPC1)
             else:
-                app_logger.info(f"Subscriber {imsi} not found in {EPC2}")
+                app_logger.info(f"Incorrect event subscriber sent or no imsi found in the data")
         else:
             app_logger.info(f"POST request from unknown server or does not match subscriber event {request.remote_addr}")
         return '', 200
@@ -97,5 +100,4 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
 
